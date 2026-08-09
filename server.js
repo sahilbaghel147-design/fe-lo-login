750c2070b860fda3da7bbe18baa93e1d6977bc83
sahilbaghel147-design <sahilbaghel147@gmail.com>
2026-08-09 14:39:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,20 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+// /collect endpoint to receive data
 app.post('/collect', (req, res) => {
   const data = req.body;
   console.log("Received data:", data);
+  // Save data to a file or database
   res.status(200).send("Data collected successfully!");
 });
 
+// /no_think endpoint to make it look like a normal website
 app.get('/no_think', (req, res) => {
   res.status(200).send("This is a normal website. No data is being collected.");
 });
 
+// /download endpoint to allow users to download collected data
 app.get('/download', (req, res) => {
   res.download('collected_data.json', 'collected_data.json', (err) => {
     if (err) {
